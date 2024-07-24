cde8925bed63ee77a56493bd20fdde8b9fe966c1
erculesferraz <hercules.ferrazportella@gmail.com>
2024-07-24 16:30:54 -0300

    Novo Repositório

---
+++ assets/js/mobile.js
@@ -0,0 +1,47 @@
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks, footer) {
+      this.mobileMenu = document.querySelector(mobileMenu);
+      this.navList = document.querySelector(navList);
+      this.navLinks = document.querySelectorAll(navLinks);
+      this.footer = document.querySelector(footer); // new property to store the footer element
+      this.activeClass = "active";
+  
+      this.handleClick = this.handleClick.bind(this);
+    }
+  
+    animateLinks() {
+      this.navLinks.forEach((link, index) => {
+        link.style.animation
+         ? (link.style.animation = "")
+          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+              index / 7 + 0.3
+            }s`);
+      });
+    }
+  
+    handleClick() {
+      this.navList.classList.toggle(this.activeClass);
+      this.mobileMenu.classList.toggle(this.activeClass);
+      this.animateLinks();
+      this.footer.classList.toggle("hidden"); // toggle the hidden class on the footer
+    }
+  
+    addClickEvent() {
+      this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+  
+    init() {
+      if (this.mobileMenu) {
+        this.addClickEvent();
+      }
+      return this;
+    }
+  }
+  
+  const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+    ".footer" // pass the footer selector to the constructor
+  );
+  mobileNavbar.init();
